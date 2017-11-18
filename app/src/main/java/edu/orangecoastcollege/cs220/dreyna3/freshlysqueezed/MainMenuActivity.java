4b6a8420de2303f631c5eae65dc3c7aa430fbabe
Daniel Reyna <danielreyna214@gmail.com>
2017-11-18 13:10:51 -0800

    added java activities for the xml layouts

---
+++ app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/MainMenuActivity.java
@@ -0,0 +1,15 @@
+package edu.orangecoastcollege.cs220.dreyna3.freshlysqueezed;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Daniel on 11/18/2017.
+ */
+
+public class MainMenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
