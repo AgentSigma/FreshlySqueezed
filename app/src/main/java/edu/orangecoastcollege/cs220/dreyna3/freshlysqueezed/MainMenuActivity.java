b81ebc24b2067964eea7d30d1507a7b15a8ff6d1
Daniel Reyna <danielreyna214@gmail.com>
2017-11-20 19:51:36 -0800

    matched themes to correctly display on device, also some buttons now clickable

--- app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/MainMenuActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/MainMenuActivity.java
@@ -1,15 +1,23 @@
 package edu.orangecoastcollege.cs220.dreyna3.freshlysqueezed;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Daniel on 11/18/2017.
- */
+import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+
+    public void logOutClick(View view) {
+        finish();
+    }
+
+    public void searchClick(View view) {
+        startActivity(new Intent(this, SearchActivity.class));
     }
 }
