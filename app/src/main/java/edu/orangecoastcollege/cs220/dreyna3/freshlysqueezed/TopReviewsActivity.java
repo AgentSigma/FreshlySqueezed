1ea0e60e63a96684270af0681e29e79cf844dae8
anamvari <alexnamvari@yahoo.com>
2017-11-28 13:02:37 -0800

    New Movie object was created along with TopReviewsActivity and interface layout. Contain List that needs to be completed.

---
+++ app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/TopReviewsActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs220.dreyna3.freshlysqueezed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopReviewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_top_reviews);
+    }
+}
