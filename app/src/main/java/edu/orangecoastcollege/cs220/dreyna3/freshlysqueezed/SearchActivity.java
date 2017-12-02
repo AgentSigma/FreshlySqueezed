305f215151f03702f10565a42328b523b6dad35b
TongOCC <agdtev@gmail.com>
2017-12-02 14:43:23 -0800

    added image uri passing

--- app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/SearchActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs220/dreyna3/freshlysqueezed/SearchActivity.java
@@ -1,9 +1,11 @@
 package edu.orangecoastcollege.cs220.dreyna3.freshlysqueezed;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.io.IOException;
@@ -14,14 +16,21 @@ public class SearchActivity extends AppCompatActivity {
     private ListView movieSearchListView;
     private MovieListAdapter mMovieListAdapter;
     private List<Movie> allMoviesList;
+    private Profile userProfile;
+    private ImageView profileImage;
+    private Uri imageUri;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        movieSearchListView = (ListView) findViewById(R.id.movieSearchListView);
-
+        movieSearchListView = (ListView) findViewById(R.id.MovieSearchListView);
+        profileImage=(ImageView) findViewById(R.id.searchUserProfileImageView);
+        userProfile= getIntent().getParcelableExtra("userProfile");
+        imageUri= Uri.parse(getIntent().getStringExtra("userImage"));
+        profileImage.setImageURI(imageUri);
         try {
             allMoviesList = MovieJSONLoader.loadJSONFromAsset(this);
         } catch (IOException e) {
